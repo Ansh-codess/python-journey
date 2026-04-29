f8675922f4ce3bee1f1b6ac0ea3240d0292de6d8
Ansh <coderansh2007@gmail.com>
2026-04-29 22:43:41 +0530

    day 4 python practice

---
+++ day4_loops.py
@@ -0,0 +1,6 @@
+numbers=[2,2,2,2,7]
+for x in numbers:
+    for y in range(x):
+        print("x",end="")
+    print()
+
---
+++ day5_lists.py
@@ -0,0 +1,6 @@
+numbers=[1,2,5,8,9,4,3,0]
+max=0
+for i in numbers:
+    if i>max:
+        max=i
+print(f'the biggest number in the list={max}')
